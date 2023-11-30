4a6d9baad28587cdff85ec4413c51cb6e531433b
timomac <>
2023-11-30 09:41:16 +0800

    通过脚本release.sh发版

--- source/src/components/Widget_Xbbcq.tsx
+++ source/src/components/Widget_Xbbcq.tsx
@@ -1,12 +1,39 @@
-import { aaa } from '@/services/xbbcq_service';
-import { Col, ColorPicker, Divider, Row, Space } from 'antd';
+import { heroArray } from '@/services/xbbcq_service';
+import { Col, Divider, Row, Space } from 'antd';
 import React, { useEffect, useRef, useState } from 'react';
 
-export default function Widget_Xbbcq() {
+function getImageUrl(imgName: string) {
+	return new URL(`../images/xbbcq/hero/${imgName}`, import.meta.url).href;
+}
 
+export default function Widget_Xbbcq() {
 	return (
-		<>
-			<img src={aaa()} alt=''/>
-		</>
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'row',
+				alignItems: 'center',
+				flexWrap: 'wrap',
+				width: '100%',
+			}}
+		>
+			{heroArray.map((hero, index) => {
+				return (
+					<div
+						key={hero.name}
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+							justifyContent: 'center',
+                            marginRight: 10,
+						}}
+					>
+						<img src={getImageUrl(hero.imgName)} alt="" />
+						<h2>{hero.name}</h2>
+					</div>
+				);
+			})}
+		</div>
 	);
 }
--- source/src/services/xbbcq_service.ts
+++ source/src/services/xbbcq_service.ts
@@ -1,5 +1,368 @@
-function getImageUrl(imgName: string) {
-	return new URL(`../images/xbbcq/hero/${imgName}`, import.meta.url).href;
-}
-
-export const aaa = () => getImageUrl('AM.jpg');
+export const heroArray = [
+    {
+        name: '敌法',
+        name2: 'df',
+        imgName: 'AM.jpg',
+    },
+    {
+        name: '斧王',
+        name2: 'fw',
+        imgName: 'Axe.jpg',
+    },
+    {
+        name: '刚被猪',
+        name2: 'gbz',
+        imgName: 'BB.jpg',
+    },
+    {
+        name: '骨弓',
+        name2: 'gg',
+        imgName: 'Bone.jpg',
+    },
+    {
+        name: '育母蜘蛛',
+        name2: 'ymzz',
+        imgName: 'BR.jpg',
+    },
+    {
+        name: '发条',
+        name2: 'ft',
+        imgName: 'CG.jpg',
+    },
+    {
+        name: '冰女',
+        name2: 'bn',
+        imgName: 'CM.jpg',
+    },
+    {
+        name: '船长',
+        name2: 'cz',
+        imgName: 'Coco.jpg',
+    },
+    {
+        name: '人马',
+        name2: 'rm',
+        imgName: 'CW.jpg',
+    },
+    {
+        name: '舞姬',
+        name2: 'wj',
+        imgName: 'Dancer.jpg',
+    },
+    {
+        name: '末日',
+        name2: 'mr',
+        name3: 'doom',
+        imgName: 'DOOM.jpg',
+    },
+    {
+        name: '死亡先知',
+        name2: 'swxz',
+        imgName: 'DP.jpg',
+    },
+    {
+        name: '小黑',
+        name2: 'xh',
+        imgName: 'DR.jpg',
+    },
+    {
+        name: '灰烬之灵',
+        name2: 'hjzl',
+        name3: '火猫',
+        name4: 'hm',
+        imgName: 'EM.jpg',
+    },
+    {
+        name: '小鹿',
+        name2: 'xl',
+        imgName: 'Ench.jpg',
+    },
+    {
+        name: '神牛',
+        name2: 'sn',
+        imgName: 'ES.jpg',
+    },
+    {
+        name: '神灵武士',
+        name2: 'slws',
+        imgName: 'Huskar.jpg',
+    },
+    {
+        name: '小精灵',
+        name2: 'xjl',
+        imgName: 'IO.jpg',
+    },
+    {
+        name: '剑圣',
+        name2: 'js',
+        imgName: 'JUGG.jpg',
+    },
+    {
+        name: '光法',
+        name2: 'gf',
+        imgName: 'KOTL.jpg',
+    },
+    {
+        name: '猴子',
+        name2: 'hz',
+        imgName: 'Lancer.jpg',
+    },
+    {
+        name: '军团指挥',
+        name2: 'jtzh',
+        imgName: 'LC.jpg',
+    },
+    {
+        name: '巫妖',
+        name2: 'wy',
+        imgName: 'Lich.jpg',
+    },
+    {
+        name: '火女',
+        name2: 'hn',
+        imgName: 'Lina.jpg',
+    },
+    {
+        name: '恶魔巫师',
+        name2: 'emws',
+        imgName: 'Lion.jpg',
+    },
+    {
+        name: '斧王',
+        name2: 'fw',
+        imgName: 'Axe.jpg',
+    },
+    {
+        name: '敌法',
+        name2: 'df',
+        imgName: 'AM.jpg',
+    },
+    {
+        name: '黑暗骑士',
+        name2: 'haqs',
+        imgName: 'LOA.jpg',
+    },
+    {
+        name: '月骑',
+        name2: 'yq',
+        imgName: 'Luna.jpg',
+    },
+    {
+        name: '狼人',
+        name2: 'lr',
+        imgName: 'LYC.jpg',
+    },
+    {
+        name: '美杜莎',
+        name2: 'mds',
+        name3: '一姐',
+        name4: 'yj',
+        imgName: 'Med.jpg',
+    },
+    {
+        name: '云游武僧',
+        name2: 'yyws',
+        imgName: 'Monk.jpg',
+    },
+    {
+        name: '小娜迦',
+        name2: 'xnj',
+        imgName: 'Naga.jpg',
+    },
+    {
+        name: '白银刺客',
+        name2: 'byck',
+        imgName: 'Nana.jpg',
+    },
+    {
+        name: '死灵法师',
+        name2: 'slfs',
+        imgName: 'NEC.jpg',
+    },
+    {
+        name: '黑鸟',
+        name2: 'hn',
+        imgName: 'OD.jpg',
+    },
+    {
+        name: '全能骑士',
+        name2: 'qnqs',
+        imgName: 'OK.jpg',
+    },
+    {
+        name: '蓝胖',
+        name2: 'lp',
+        imgName: 'OM.jpg',
+    },
+    {
+        name: '幻影刺客',
+        name2: 'hyck',
+        name3: 'pa',
+        imgName: 'PA.jpg',
+    },
+    {
+        name: '酒仙',
+        name2: 'jx',
+        name3: '熊猫',
+        name4: 'xm',
+        imgName: 'Panda.jpg',
+    },
+    {
+        name: '凤凰',
+        name2: 'fh',
+        imgName: 'Phoenix.jpg',
+    },
+    {
+        name: '白虎',
+        name2: 'bh',
+        imgName: 'POM.jpg',
+    },
+    {
+        name: '屠芙',
+        name2: 'tf',
+        imgName: 'Pudge.jpg',
+    },
+    {
+        name: '骨法',
+        name2: 'gf',
+        imgName: 'Pugna.jpg',
+    },
+    {
+        name: '痛苦女王',
+        name2: 'tknw',
+        imgName: 'QOP.jpg',
+    },
+    {
+        name: '电魂',
+        name2: 'dh',
+        imgName: 'Razor.jpg',
+    },
+    {
+        name: '影魔',
+        name2: 'ym',
+        imgName: 'SF.jpg',
+    },
+    {
+        name: '大鱼人',
+        name2: 'dyr',
+        imgName: 'SG.jpg',
+    },
+    {
+        name: '沉默',
+        name2: 'cm',
+        imgName: 'Sil.jpg',
+    },
+    {
+        name: '天怒法师',
+        name2: 'tnfs',
+        imgName: 'SM.jpg',
+    },
+    {
+        name: '火枪',
+        name2: 'hq',
+        imgName: 'Sniper.jpg',
+    },
+    {
+        name: '骨王',
+        name2: 'gw',
+        imgName: 'SNK.jpg',
+    },
+    {
+        name: '暗牧',
+        name2: 'am',
+        imgName: 'SP.jpg',
+    },
+    {
+        name: '圣堂刺客',
+        name2: 'stck',
+        name3: 'ta',
+        imgName: 'TA.jpg',
+    },
+    {
+        name: '灵魂守卫',
+        name2: 'lhsw',
+        name3: 'tb',
+        imgName: 'TB.jpg',
+    },
+    {
+        name: '潮汐',
+        name2: 'cx',
+        imgName: 'TH.jpg',
+    },
+    {
+        name: '双头龙',
+        name2: 'stl',
+        imgName: 'THD.jpg',
+    },
+    {
+        name: '小小',
+        name2: 'xx',
+        imgName: 'Tiny.jpg',
+    },
+    {
+        name: '德鲁伊',
+        name2: 'dly',
+        imgName: 'TitanDruid.jpg',
+    },
+    {
+        name: '战神',
+        name2: 'zs',
+        imgName: 'TitanMars.jpg',
+    },
+    {
+        name: '修补匠',
+        name2: 'xbj',
+        name3: 'tk',
+        imgName: 'TK.jpg',
+    },
+    {
+        name: '巨魔',
+        name2: 'jm',
+        imgName: 'Troll.jpg',
+    },
+    {
+        name: '海民',
+        name2: 'hm',
+        imgName: 'Tusk.jpg',
+    },
+    {
+        name: '尸王',
+        name2: 'sw',
+        imgName: 'UD.jpg',
+    },
+    {
+        name: '拍拍熊',
+        name2: 'ppx',
+        imgName: 'Ursa.jpg',
+    },
+    {
+        name: '亚龙',
+        name2: 'yl',
+        imgName: 'Viper.jpg',
+    },
+    {
+        name: '复仇',
+        name2: 'fc',
+        imgName: 'VS.jpg',
+    },
+    {
+        name: '巫医',
+        name2: 'wy',
+        imgName: 'WD.jpg',
+    },
+    {
+        name: '术士',
+        name2: 'ss',
+        imgName: 'WL.jpg',
+    },
+    {
+        name: '风行',
+        name2: 'fx',
+        imgName: 'WR.jpg',
+    },
+    {
+        name: '宙斯',
+        name2: 'zs',
+        imgName: 'Zeus.jpg',
+    },
+]
